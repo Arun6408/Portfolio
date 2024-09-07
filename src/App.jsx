9d4fe3f873fe981404cd908e7f3d7c76717b3eaf
Ch Arun <challaarunkumar007@gmail.com>
2024-09-08 00:14:35 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Contact from './components/Contact/Contact'
 import Hero from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
 import Projects from './components/Projects/Projects'
-import Skills from './components/Skills/skills'
+import Skills from './components/Skills/Skills'
 
 function App() {
 
